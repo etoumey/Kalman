56afec325217ed4a873f352918e7ac01ed478802
etoumey <etoumey@gmail.com>
2018-09-02 20:11:18 +0000

    Love the commit

--- accel.py
+++ accel.py
@@ -2,6 +2,7 @@ from mpu6050 import mpu6050
 from time import sleep
 import math
 #Put mpu6050 in corresponding ports to first 4 pins
+outFile = open('outFile.dat','w')
 
 sensor = mpu6050(0x68)
 while True:
@@ -13,6 +14,8 @@ while True:
 	print "xRot: %s" % dataGyro['x']
         print "yRot: %s" % dataGyro['y']
         print "zRot: %s\n" % dataGyro['z']
+	outFile.write("%s %s %s %s %s %s\n" % (dataAccel['x'], dataAccel['y'], dataAccel['z'], dataGyro['x'], dataGyro['y'], dataGyro['z']))
+	
 
-	sleep(1)
+outFile.close()
 
