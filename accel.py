d683aae305466e8dede73bb9e1de1c60502656da
etoumey <etoumey@gmail.com>
2018-09-02 19:52:16 +0000

    gyro and accel at once

--- accel.py
+++ accel.py
@@ -1,26 +1,18 @@
 from mpu6050 import mpu6050
 from time import sleep
 import math
-
-outFile = open('acceleration.dat', 'w')
-outFile.write('# x y z mag\n')
+#Put mpu6050 in corresponding ports to first 4 pins
 
 sensor = mpu6050(0x68)
 while True:
 	dataAccel = sensor.get_accel_data()
+	dataGyro = sensor.get_gyro_data() 
         print "x: %s" % dataAccel['x']
         print "y: %s" % dataAccel['y']
         print "z: %s\n" % dataAccel['z']
- 
-        mag = math.sqrt(dataAccel['x']**2 + dataAccel['y']**2 + dataAccel['z']**2)
-
-	if ( mag > 12)
-        
-
-	outFile.write("%s %s %s %s\n" % (dataAccel['x'], dataAccel['y'], dataAccel['z'], mag))
-
-
+	print "xRot: %s" % dataGyro['x']
+        print "yRot: %s" % dataGyro['y']
+        print "zRot: %s\n" % dataGyro['z']
 
 	sleep(1)
 
-outFile.close()
